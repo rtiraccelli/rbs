2d6208c093060687b38a3539f92b2b2a9385af6c
Roberto Tirado <rtiraccelli@gmail.com>
2025-06-19 14:02:04 -0400

    Lesson 4, class 1 update

--- RBS_Python_Corso/Exercises_Lesson3.py
+++ RBS_Python_Corso/Exercises_Lesson3.py
@@ -27,10 +27,65 @@ import matplotlib.pyplot as plt
 #plt.show()
 
 # Data for histogram
-data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 10]
+#data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 10]
+#plt.hist(data, bins=5, color='purple')
+#plt.xlabel('Data')
+#plt.ylabel('Frequency')
+#plt.title('Histogram')
+#plt.show()
 
-plt.hist(data, bins=5, color='purple')
-plt.xlabel('Data')
-plt.ylabel('Frequency')
-plt.title('Histogram')
-plt.show()
+
+# Creating 3D Plots
+#from mpl_toolkits.mplot3d import Axes3D
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+# Data for 3D plot
+#x = [1, 2, 3, 4, 5]
+#y = [2, 3, 5, 7, 11]
+#z = [1, 4, 9, 16, 25]
+
+#n = [2, 4, 7, 8, 9]
+#o = [2, 3, 5, 7, 11]
+#p = [3, 9, 16, 25, 36]
+
+#ax.plot(x, y, z, color='blue', label='Line 1')
+#ax.plot(n, o, p, color='green', label='Line 2')
+#ax.set_xlabel('X-axis')
+#ax.set_ylabel('Y-axis')
+#ax.set_zlabel('Z-axis')
+#ax.set_title('3D Plot')
+#plt.show()
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the tips dataset
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+# Box plot with Seaborn
+#sns.boxplot(x='day', y='total_bill', data=tips)
+#plt.title('Box Plot of Total Bill by Day')
+#plt.show()
+
+
+# Pair plot with Seaborn
+#sns.pairplot(tips)
+#plt.title('Pair Plot of Tips Dataset')
+#plt.show()
+# Select only numeric columns for correlation matrix
+#numeric_tips = tips.select_dtypes(include=['float64', 'int64'])
+# Correlation matrix
+#corr = numeric_tips.corr()
+# Heatmap with Seaborn
+#sns.heatmap(corr, annot=False, cmap='inferno', center=0)
+#plt.title('Heatmap of Correlation Matrix')
+#plt.show()
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Scatter plot with Plotly
+fig = px.histogram(tips, x='total_bill', y='tip', title='Scatter Plot of Total Bill vs Tip')
+fig.show()
