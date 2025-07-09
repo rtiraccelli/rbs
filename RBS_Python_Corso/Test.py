ff2b4a7db9972f3a0aa893333707fa173c64f4c1
Roberto Tirado <rtiraccelli@gmail.com>
2025-07-09 11:31:42 -0400

    Evaluation exercices

---
+++ RBS_Python_Corso/Test.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+""""
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+
+my_list = [1, 2, 3, 4]
+print(my_list[1:3])
+
+np.zeros((3, 4))
+print(np.zeros((3, 4)))
+
+x = 5
+
+if x > 3:
+    print("Greater than 3")
+else:
+    print("Less than or equal to 3")
+"""
+
+x = [1, 2, 3]
+
+print(len(x))
